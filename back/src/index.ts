1ab2d70b5eb32d45ba53b408f5b9cba012737a7f
Natalia-Barragan <natalia.barragannb@gmail.com>
2026-01-16 16:44:23 -0300

    conf bd

--- back/src/index.ts
+++ back/src/index.ts
@@ -5,9 +5,14 @@ import { AppDataSource } from './config/data-sources';
 
 AppDataSource.initialize()
   .then(() => {
-    server.listen(config.PORT);     
+    console.log("Database connected successfully");
+    server.listen(config.PORT, () => {
+      console.log(`Server listening on port ${config.PORT}`);
+    });
   })
   .catch((error) => {
+    console.error("Error connecting to the database:", error);
+    process.exit(1);
   });
 
 
