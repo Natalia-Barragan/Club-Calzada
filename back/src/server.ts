cba81ba5e718bcf6855ce915fcdb75b28aaae23b
Natalia-Barragan <natalia.barragannb@gmail.com>
2026-01-17 17:25:48 -0300

    front de dashmin

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,14 +1,23 @@
-import express, { Application} from 'express';
+import express, { Application } from 'express';
 import router from './routes/indexRoutes';
 import morgan from 'morgan';
 import cors from 'cors';
 
 
-const server: Application  = express();
+const server: Application = express();
+
+server.use(cors());
 
 server.use(express.json());
 server.use(morgan('dev'));
-server.use(cors());
+// Manual CORS to rule out library issues
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 server.use(router);
 
